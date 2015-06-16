7fb2cb17fa98596072805b9b23b389cac837d63a
Daria Jung <djj2115cu@gmail.com>
2015-06-16 10:46:12 -0700

    (feat): add typing specs
    add test in gulpfile which will compile a basic TS file with generated angular2.d.ts to ensure generated d.ts is valid syntactic TS

--- modules/angular2/http.ts
+++ modules/angular2/http.ts
@@ -16,31 +16,43 @@ export {Request} from 'angular2/src/http/static_request';
 export {Response} from 'angular2/src/http/static_response';
 
 export {Http, XHRBackend, XHRConnection, BaseRequestOptions, RequestOptions, HttpFactory};
-export {IHttp} from 'angular2/src/http/interfaces';
+export {
+  IHttp,
+  IRequestOptions,
+  IRequest,
+  IResponse,
+  Connection,
+  ConnectionBackend
+} from 'angular2/src/http/interfaces';
 export {Headers} from 'angular2/src/http/headers';
 
-/**
- * Provides a basic set of injectables to use the {@link Http} service in any application.
- *
- * #Example
- *
- * ```
- * import {httpInjectables, Http} from 'angular2/http';
- * @Component({selector: 'http-app', appInjector: [httpInjectables]})
- * @View({template: '{{data}}'})
- * class MyApp {
- *   constructor(http:Http) {
- *     http.request('data.txt').subscribe(res => this.data = res.text());
- *   }
- * }
- * ```
- *
- */
-export var httpInjectables: List<any> = [
-  bind(BrowserXHR)
-      .toValue(BrowserXHR),
-  XHRBackend,
-  BaseRequestOptions,
-  bind(HttpFactory).toFactory(HttpFactory, [XHRBackend, BaseRequestOptions]),
-  Http
-];
+export * from 'angular2/src/http/enums';
+export {
+  URLSearchParams
+} from 'angular2/src/http/url_search_params'
+
+    /**
+     * Provides a basic set of injectables to use the {@link Http} service in any application.
+     *
+     * #Example
+     *
+     * ```
+     * import {httpInjectables, Http} from 'angular2/http';
+     * @Component({selector: 'http-app', appInjector: [httpInjectables]})
+     * @View({template: '{{data}}'})
+     * class MyApp {
+     *   constructor(http:Http) {
+     *     http.request('data.txt').subscribe(res => this.data = res.text());
+     *   }
+     * }
+     * ```
+     *
+     */
+    export var httpInjectables: List<any> = [
+      bind(BrowserXHR)
+          .toValue(BrowserXHR),
+      XHRBackend,
+      BaseRequestOptions,
+      bind(HttpFactory).toFactory(HttpFactory, [XHRBackend, BaseRequestOptions]),
+      Http
+    ];
